c28bb20c16d0ef9051cb9417c0b44ad0a6894080
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-10-02 09:30:48 +0300

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,22 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
   };
+  componentDidMount() {
+    const contactsJson = localStorage.getItem('contacts');
+    const contactsParse = JSON.parse(contactsJson) ?? [];
+    this.setState({ contacts: contactsParse });
+  }
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      const contactsJson = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', contactsJson);
+    }
+  }
 
   handlAddContact = contactData => {
     const { contacts } = this.state;
@@ -45,6 +51,7 @@ export class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+
   render() {
     const { contacts, filter } = this.state;
     const filteredContacts = contacts.filter(contact =>
